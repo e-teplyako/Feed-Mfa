a8cb827e936ecc1ab55027446b616bd1cd3157ca
Ekaterina Teplyakova <ekaterina.teplyakova93@gmail.com>
2020-12-25 17:25:56 +0300

    fixed bug with breaking whole feed on one url exception

--- app/src/main/java/misc/teplyakova/rssmfa/RssModel.java
+++ app/src/main/java/misc/teplyakova/rssmfa/RssModel.java
@@ -5,10 +5,14 @@ import android.os.AsyncTask;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.feed.synd.SyndFeedImpl;
+import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
+import com.rometools.rome.io.impl.XmlFixerReader;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
@@ -16,6 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
+import cz.msebera.android.httpclient.client.ClientProtocolException;
 import cz.msebera.android.httpclient.client.methods.CloseableHttpResponse;
 import cz.msebera.android.httpclient.client.methods.HttpGet;
 import cz.msebera.android.httpclient.client.methods.HttpUriRequest;
@@ -46,22 +51,31 @@ public class RssModel {
 			List<SyndEntry> entries = new ArrayList();
 			aggregatedFeed.setEntries(entries);
 
-			try {
+
 				for (URL url : lists[0].keySet()) {
 					CloseableHttpClient client = HttpClients.createMinimal();
-					HttpUriRequest request = new HttpGet(url.toURI());
-					CloseableHttpResponse response = client.execute(request);
-					InputStream is = response.getEntity().getContent();
+					InputStream is = null;
+					try {
+						HttpUriRequest request = new HttpGet(url.toURI());
+						CloseableHttpResponse response = client.execute(request);
+						is = response.getEntity().getContent();
+					}
+					catch (URISyntaxException | IOException e) {
+						e.printStackTrace();
+					}
 					SyndFeedInput input = new SyndFeedInput();
-					SyndFeed feed = input.build(new XmlReader(is));
+					SyndFeed feed = new SyndFeedImpl();
+					try {
+						feed = input.build(new XmlReader(is));
+					}
+					catch (IOException | FeedException e) {
+						e.printStackTrace();
+					}
 					for (SyndEntry entry : feed.getEntries()) {
 						RssItem item = new RssItem(lists[0].get(url), entry.getTitle(), entry.getDescription().getValue(), entry.getPublishedDate(), entry.getLink());
 						rssItems.add(item);
 					}
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 			return rssItems;
 		}
 
